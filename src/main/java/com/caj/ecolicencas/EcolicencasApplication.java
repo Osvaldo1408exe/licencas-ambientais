62cef1ff9468e06b16d4c0411c27587d260e00b4
Osvaldo1408exe <Osvaldo1408.exe@gmail.com>
2024-10-08 14:24:25 -0300

    feat: alterado para spring boot

---
+++ src/main/java/com/caj/ecolicencas/EcolicencasApplication.java
@@ -0,0 +1,13 @@
+package com.caj.ecolicencas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcolicencasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcolicencasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caj/ecolicencas/EcolicencasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caj.ecolicencas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcolicencasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
