23342c955c6d9a3edc36de3a0ec9b0068b8f272f
Osvaldo1408exe <Osvaldo1408.exe@gmail.com>
2024-10-30 17:30:59 -0300

    feat: metodo de busca pelo parametro de descrição

--- src/main/java/com/caj/ecolicencas/repository/SituacaoLicencaRepository.java
+++ src/main/java/com/caj/ecolicencas/repository/SituacaoLicencaRepository.java
@@ -4,4 +4,6 @@ import com.caj.ecolicencas.model.entities.SituacaoLicenca;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SituacaoLicencaRepository extends JpaRepository<SituacaoLicenca,Integer> {
+
+    SituacaoLicenca findByDescricao(String descricao);
 }
